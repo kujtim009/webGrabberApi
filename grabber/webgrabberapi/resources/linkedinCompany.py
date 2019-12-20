9e5e583cf0491aae63bc6388797df8d4c426ab76
Kujtim Durguti <kujtim009@gmail.com>
2019-12-20 14:36:21 +0100

    modifying linkedin company scrapper

--- grabber/webgrabberapi/resources/linkedinCompany.py
+++ grabber/webgrabberapi/resources/linkedinCompany.py
@@ -1,4 +1,4 @@
-from scrape_linkedin import CompanyScraper
+from scrape_linkedin import CompanyScraper, Scraper
 import pandas as pd
 
 
@@ -6,18 +6,18 @@ class companyLinkedin(object):
     def __init__(self, cpNames):
         self.cpNames = cpNames
         self.companyData = []
+        self.coockie = 'AQEDAS6InWoFvL_lAAABbvSnHPwAAAFvGLOg_E0AAfV7ciBpclPS8rTQsqWivxB0WMh_YqPM8UsjvlkCEC-NYRxFjjDOV6c65SgxklNeSRWNfeSeq4IVHYYQOaPOVwVZxil7XrhLV-wq5z4f2ZzpRdHF'
 
     def getCompanyData(self):
-        with CompanyScraper(cookie='AQEDAS6InWoCpYoDAAABbtrZyx8AAAFu_uZPH00Aq-Oq6Jl03e0UOg0qtlrADIM9OoDsKDBN_-4bHeRmqC4-0fJ-GSv9F66-O8SZY2GpuaojYtoKPvpU32fmdVXwxr-IzYl_otB8ADTv5BuayrVdQ5d_') as scraper:
+        with CompanyScraper(cookie=self.coockie) as scraper:
             for name in self.cpNames:
                 try:
                     overview = scraper.scrape(company=name).overview
                     overview['company_name'] = name
-                    self.company_data.append(overview)
+                    self.companyData.append(overview)
                 except:
                     pass
 
     def getJson(self):
         df = pd.DataFrame(self.companyData)
-        df.to_csv('out.csv', index=False)
-        return df.to_json()
+        return df.to_json(orient='records')
--- grabber/webgrabberapi/views.py
+++ grabber/webgrabberapi/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView 
 from rest_framework.response import Response
 from .resources.myrequest import CtxRequest
+from .resources.linkedinCompany import companyLinkedin
 import requests
 
 
@@ -18,10 +19,9 @@ class UrlGrabberView(APIView):
 
 class linkedinCompanyGrabber(APIView):
     def get(self, request):
-        url = self.request
-        print("Parsing Started!")
-        myRequest = CtxRequest(url)
-        result = myRequest.getEmails()
-        # req = requests.get(url)
-        # queryset = Ulset.objects.all().update(statusi=False)
-        return Response({"Emails": result["emails"], "Page_status": result["status"], "url_validity": result["urlValidity"]})
+        cpNames = self.request.data["names"]
+        linkedin = companyLinkedin(cpNames)
+        linkedin.getCompanyData()
+        print("Request data: ", cpNames)
+        return Response(linkedin.getJson())
+
